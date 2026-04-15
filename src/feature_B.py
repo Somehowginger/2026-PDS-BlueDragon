7317f9bbf61372e572b3b547ab746bd5db40f630
Mark <kr.mark2005@gmail.com>
2026-04-15 10:37:49 +0200

    fixed feature B

--- src/feature_B.py
+++ src/feature_B.py
@@ -3,10 +3,11 @@ import numpy as np
 
 def get_compactness(mask):
     # mask = color.rgb2gray(mask)
+    mask = (mask > 127)
     area = np.sum(mask)
 
     struct_el = morphology.disk(3)
     mask_eroded = morphology.binary_erosion(mask, struct_el)
-    perimeter = np.sum(mask - mask_eroded)
+    perimeter = np.sum(mask ^ mask_eroded)
 
-    return perimeter**2 / (4 * np.pi * area)
+    return round(float(perimeter**2 / (4 * np.pi * area)),3)
