e5123482717110cdb3e86c588612d7b451f3b24f
Mark <kr.mark2005@gmail.com>
2026-04-15 12:12:02 +0200

    fixed feature extraction and feature B

--- src/extract_features.py
+++ src/extract_features.py
@@ -1,13 +1,11 @@
 import pandas as pd
 from pathlib import Path
-from feature_A import get_assymetry
-from feature_B import get_compactness
-#from feature_C import get_hsv_means
+from skimage import io
+import feature_A
+import feature_B
 
 def process_csv(input_file, output_file):
     df = pd.read_csv(input_file)
-
-    masks_path = Path(__file__).resolve().parent.parent / "data" / "masks"
 
     def process_row(row):
         img_name = row['img_id']
@@ -15,20 +13,21 @@ def process_csv(input_file, output_file):
         parts = img_name.split('_')
         id_value = f"{parts[0]}_{parts[1]}"
 
-        image_path = masks_path / img_name
+        masks_path = Path(__file__).resolve().parent.parent / "data" / "masks"
+        mask_name = img_name[:-4] + "_mask" + img_name[-4:]
+        mask_path = masks_path / mask_name
 
-        if not image_path.exists():
+        if not mask_path.exists():
             print(f"Missing image: {img_name}")
             return pd.Series([id_value, None, None, None])
 
-        assymetry = get_assymetry(image_path)
-        compactness = get_compactness(image_path)
-        #colour = get_hsv_means()
+        mask = io.imread(mask_path)
+        compactness = feature_B.get_compactness(mask)
 
-        return pd.Series([id_value, assymetry, compactness, None])
-    
-    df[['ID', 'feature_A', 'feature_B', 'feature_C']] = df.apply(process_row, axis=1)
+        return pd.Series([id_value, None, compactness, None])
 
-    df[['ID', 'feature_A', 'feature_B', 'feature_C', ]].to_csv(output_file, index=False)
+    result = df.apply(process_row, axis=1)
+    result.columns = ['ID', 'feature_A', 'feature_B', 'feature_C']
+    result.to_csv(output_file, index=False)
 
 process_csv(Path(__file__).resolve().parent.parent / "data" / "features.csv", 'output.csv')
