7b6652f522eb0274f599592cf8c974107ff442df
Jadbinder Singh Toor <jadbindertoor@gmail.com>
2026-04-15 12:21:40 +0200

    Feature A - Update part 2

--- src/feature_A.py
+++ src/feature_A.py
@@ -1,6 +1,16 @@
 import numpy as np
 from skimage.transform import rotate
-from skimage.util import crop
+
+def crop(mask):
+    # find where mask is non-zero (lesion pixels)
+    y, x = np.nonzero(mask)
+
+    # get bounding box
+    y_min, y_max = y.min(), y.max()
+    x_min, x_max = x.min(), x.max()
+
+    # crop
+    return mask[y_min:y_max+1, x_min:x_max+1]
 
 def get_asymmetry(mask):
     scores = []
@@ -12,3 +22,4 @@ def get_asymmetry(mask):
         mask = rotate(mask, 30)
 
     return sum(scores) / len(scores)
+
